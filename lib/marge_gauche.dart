289c5e9c8fde2c080030def886d04b245e48e575
Lucreciaaaa <lfodouop@gmail.com>
2024-03-17 22:34:33 +0100

    ajouts

--- lib/marge_gauche.dart
+++ lib/marge_gauche.dart
@@ -8,51 +8,38 @@ class MargeGauche extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Conteneur avec l'effet d'ombre
-        Positioned(
-          left: 0,
-          child: Container(
-            width: 150, // Largeur de la marge gauche
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5), // Couleur de l'ombre
-                  spreadRadius: 6, // Étendue de l'ombre
-                  blurRadius: 8, // Flou de l'ombre
-                  offset: Offset(-4, 0), // Décalage de l'ombre (vers la gauche)
-                ),
-              ],
+    return Container(
+      width: 160, // Largeur de la marge gauche
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.all(18.0), // Ajout de padding pour espacement interne
+      decoration: BoxDecoration(
+        color: AppColors().bleuClair, // Couleur bleue
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5), // Couleur de l'ombre noire
+            blurRadius: 4, // Flou de l'ombre
+            offset: Offset(0, -3), // Décalage de l'ombre (vers la droite)
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Learning Navigator',
+            style: TextStyle(
+              color: AppColors().grisFonce, // Couleur du texte en gris
+              fontWeight: FontWeight.bold,
+              fontSize: 12.0, // Taille de police
+              fontFamily: 'Regular', // Famille de polices
             ),
           ),
-        ),
-        // Conteneur principal avec le contenu
-        Container(
-          color: AppColors().bleuClair,
-          width: 170, // Largeur de la marge gauche
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.all(18.0), // Ajout de padding pour espacement interne
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Learning Navigator',
-                style: TextStyle(
-                  color: AppColors().grisFonce, // Couleur du texte en gris
-                  fontSize: 13.0, // Taille de police
-                  fontFamily: 'Regular', // Famille de polices
-                ),
-              ),
-              SizedBox(height: 16.0), // Espacement entre le texte et le reste du contenu
-              Expanded(
-                child: child, // Affichez d'autres widgets à l'intérieur si nécessaire
-              ),
-            ],
+          SizedBox(height: 16.0), // Espacement entre le texte et le reste du contenu
+          Expanded(
+            child: child, // Affichez d'autres widgets à l'intérieur si nécessaire
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
