fdd81ad295aa165df8c38744a110307a06e2c418
Lucreciaaaa <lfodouop@gmail.com>
2024-03-17 21:14:29 +0100

    ajouts

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,5 +4,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const white = Colors.white ;
   static const black = Colors.black ;
+  final bleuClair = Colors.blue.shade100 ;
+  final grisFonce = Colors.grey.shade800;
 
 }
--- lib/marge_gauche.dart
+++ lib/marge_gauche.dart
@@ -1,2 +1,35 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
+class MargeGauche extends StatelessWidget {
+  final Widget child;
+
+  MargeGauche({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors().bleuClair,
+      height: MediaQuery.of(context).size.height,
+      width: 150, // Largeur de la marge gauche
+      padding: EdgeInsets.all(16.0), // Ajout de padding pour espacement interne
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Tableau de bord',
+            style: TextStyle(
+              color: AppColors().grisFonce, // Couleur du texte en gris
+              fontSize: 13.0, // Taille de police
+              fontFamily: 'Regular', // Famille de polices
+            ),
+          ),
+          SizedBox(height: 16.0), // Espacement entre le texte et le reste du contenu
+          Expanded(
+            child: child, // Affichez d'autres widgets à l'intérieur si nécessaire
+          ),
+        ],
+      ),
+    );
+  }
+}
