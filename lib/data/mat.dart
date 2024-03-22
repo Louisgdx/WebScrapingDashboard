955dbee9fba8d0a0c00636d88d2cef8db3193999
Lucreciaaaa <lfodouop@gmail.com>
2024-03-22 10:30:38 +0100

    widget matieres ajouts +++

--- lib/data/mat.dart
+++ lib/data/mat.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:csv/csv.dart';
 
 class Mat {
-  static Future<List<String>> getMatiereList() async {
+  static Future<List<String>> getFirstElements() async {
     String fileContent = await rootBundle.loadString('assets/bdd/notes2.csv');
-    List<String> lines = fileContent.split('\n');
-    List<String> matieres = [];
-    for (String line in lines) {
-      List<String> values = line.split(';');
-      if (values.isNotEmpty) {
-        matieres.add(values[0]);
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(fileContent);
+    List<String> firstElements = [];
+
+    for (List<dynamic> row in csvTable) {
+      if (row.isNotEmpty) {
+        firstElements.add(row.first.toString().trim());
       }
     }
-    return matieres;
+
+    return firstElements;
   }
+
+
+  static Future<List<List<String>>> getNotes() async {
+    String fileContent = await rootBundle.loadString('assets/bdd/notes2.csv');
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(fileContent);
+    List<List<String>> notes = [];
+
+    for (List<dynamic> row in csvTable) {
+      List<String> formattedRow = row.map((cell) => cell.toString().trim()).toList();
+      notes.add(formattedRow);
+    }
+
+    return notes;
+  }
+
+
+  static List<double> calculateAverages(List<List<String>> notes) {
+    List<double> averages = [];
+
+    for (List<String> noteRow in notes) {
+      print('Note Row: $noteRow');
+      if (noteRow.isNotEmpty) {
+        List<double> numericNotes = noteRow.map((note) => double.tryParse(note) ?? 0.0).toList();
+        print('Numeric Notes: $numericNotes');
+        double average = numericNotes.isNotEmpty ? numericNotes.reduce((a, b) => a + b) / numericNotes.length : 0.0;
+        print('Average: $average');
+        averages.add(average);
+      } else {
+        averages.add(0.0);
+      }
+    }
+
+    return averages;
+  }
+
 }
+
+
+
--- lib/widget_edt.dart
+++ lib/widget_edt.dart
@@ -13,44 +13,45 @@ class _WidgetEdtState extends State<WidgetEdt> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.only(
-          top: 200.0, // Position verticale du widget
-          left: 140.0, // Position horizontale du widget
-        ),
-        child: MouseRegion(
-          onEnter: (event) {
-            setState(() {
-              isHovered = true;
-            });
-          },
-          onExit: (event) {
-            setState(() {
-              isHovered = false;
-            });
-          },
-          child: AnimatedContainer(
-            duration: Duration(milliseconds: 200),
-            curve: Curves.easeInOut,
-            width: isHovered ? 450.0 : 440.0, // Largeur du widget gonflé
-            height: isHovered ? 380.0 : 370.0, // Hauteur du widget gonflé
-            decoration: BoxDecoration(
-              color: AppColors().bleuClair.withOpacity(0.8), // Couleur de fond semi-transparente
-              borderRadius: BorderRadius.circular(10.0), // Coins arrondis
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1), // Couleur de l'ombre (effet de reflet)
-                  spreadRadius: -2, // Étendue de l'ombre (vers l'intérieur)
-                  blurRadius: 6, // Flou de l'ombre
-                  offset: Offset(0, 3), // Décalage de l'ombre (vers le bas)
-                ),
-              ],
+    return Column( // Encapsuler dans une Column
+      children: [
+        Container( // Utiliser Container pour margin si nécessaire
+          margin: EdgeInsets.only(
+            top: 200.0,
+            left: 140.0,
+          ),
+          child: MouseRegion(
+            onEnter: (event) {
+              setState(() {
+                isHovered = true;
+              });
+            },
+            onExit: (event) {
+              setState(() {
+                isHovered = false;
+              });
+            },
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 200),
+              curve: Curves.easeInOut,
+              width: isHovered ? 450.0 : 440.0,
+              height: isHovered ? 380.0 : 370.0,
+              decoration: BoxDecoration(
+                color: AppColors().bleuClair.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: -2,
+                    blurRadius: 6,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
             ),
-            // Vous pouvez ajouter d'autres widgets à l'intérieur de ce conteneur si nécessaire
           ),
         ),
-      ),
+      ],
     );
   }
 }
