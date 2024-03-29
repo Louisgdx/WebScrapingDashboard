546bab636d55fbbd0b7b41562fc9217bdf8f3239
Lucreciaaaa <lfodouop@gmail.com>
2024-03-29 17:58:27 +0100

    ouverture fenetre ok

--- lib/affichage_edt.dart
+++ lib/affichage_edt.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'colors.dart';
 
 class AffichageEdt extends StatelessWidget {
+  final DateTime jourSelectionne;
+
+  AffichageEdt(this.jourSelectionne);
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
       child: Container(
-        padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(100.0),
+        decoration: BoxDecoration(
+          gradient: AppColors().gradient,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Contenu de la fenêtre modale',
+              'Emploi du temps du ${DateFormat('dd/MM/yyyy').format(jourSelectionne)}',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
