bfb425c52baaeddce4dcdb9f3147c3331cd35764
Lucreciaaaa <lfodouop@gmail.com>
2024-04-08 18:52:29 +0200

    edt

--- lib/widget_edt.dart
+++ lib/widget_edt.dart
@@ -34,40 +34,44 @@ class _WidgetEdtState extends State<WidgetEdt> {
                   isHovered = false;
                 });
               },
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 200),
-                curve: Curves.easeInOut,
-                width: isHovered ? 450.0 : 440.0,
-                height: isHovered ? 380.0 : 370.0,
-                decoration: BoxDecoration(
-                  color: AppColors().bleuClair.withOpacity(0.9),
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: AppColors().grisFonce.withOpacity(0.1),
-                      spreadRadius: -2,
-                      blurRadius: 6,
-                      offset: Offset(0, 3),
+              child: Column(
+                children: [
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 200),
+                    curve: Curves.easeInOut,
+                    width: isHovered ? 450.0 : 440.0,
+                    height: isHovered ? 370.0 : 360.0,
+                    decoration: BoxDecoration(
+                      color: AppColors().bleuClair.withOpacity(0.9),
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors().grisFonce.withOpacity(0.1),
+                          spreadRadius: -2,
+                          blurRadius: 6,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: TableCalendar(
-                  focusedDay: _focusedDay,
-                  firstDay: DateTime.utc(2010, 10, 1),
-                  lastDay: DateTime.utc(2030, 12, 31),
-                  calendarFormat: _calendarFormat,
-                  headerStyle: HeaderStyle(
-                    formatButtonVisible: false,
+                    child: TableCalendar(
+                      focusedDay: _focusedDay,
+                      firstDay: DateTime.utc(2010, 10, 1),
+                      lastDay: DateTime.utc(2030, 12, 31),
+                      calendarFormat: _calendarFormat,
+                      headerStyle: HeaderStyle(
+                        formatButtonVisible: false,
+                      ),
+                      locale: 'fr_FR',
+                      startingDayOfWeek: StartingDayOfWeek.monday,
+                      onDaySelected: (selectedDay, focusedDay) {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) => AffichageEdt(selectedDay),
+                        );
+                      },
+                    ),
                   ),
-                  locale: 'fr_FR',
-                  startingDayOfWeek: StartingDayOfWeek.monday,
-                  onDaySelected: (selectedDay, focusedDay) {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) => AffichageEdt(selectedDay),
-                    );
-                  },
-                ),
+                ],
               ),
             ),
           ),
